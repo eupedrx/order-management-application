2bf8fabc34bae3b4cb169241a5c68581a449aff0
Pedro Saturno <pedro1saturno@gmail.com>
2025-12-25 18:18:04 -0300

    criação do dominio Customers

---
+++ src/main/java/com/example/testelynx/domain/Customers.java
@@ -0,0 +1,87 @@
+package com.example.testelynx.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+public class Customers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 160)
+    private String email;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+    // Relacionamentos
+
+    @JsonIgnore  // Evita o looping inifinito ao criar um relacionamento bidirecional (criar um order)
+    @OneToMany(mappedBy = "customers", cascade = CascadeType.ALL)
+    private List<Orders> orders = new ArrayList<>();
+
+
+    // Construtores
+    public Customers() {
+    }
+
+    public Customers(String name, String email, LocalDateTime createdAt, List<Orders> orders) {
+        this.name = name;
+        this.email = email;
+        this.createdAt = createdAt;
+        this.orders = orders;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<Orders> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Orders> orders) {
+        this.orders = orders;
+    }
+}
