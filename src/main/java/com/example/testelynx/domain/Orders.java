a639536be7f5ba816c595c955eb011421a5f34ee
Pedro Saturno <pedro1saturno@gmail.com>
2025-12-25 18:23:12 -0300

    estruturação e adição de metodos na entidade Orders

--- src/main/java/com/example/testelynx/domain/Orders.java
+++ src/main/java/com/example/testelynx/domain/Orders.java
@@ -1,4 +1,94 @@
 package com.example.testelynx.domain;
 
-public class Orders {
+
+
+import com.example.testelynx.domain.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Orders{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+    // Relacionamentos
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customers customers;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private OrderStatus status = OrderStatus.NEW;
+
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItems> items = new ArrayList<>();
+
+    // Construtores
+    public Orders() {
+    }
+
+    public Orders(LocalDateTime createdAt, Customers customers, OrderStatus status, List<OrderItems> items) {
+        this.createdAt = createdAt;
+        this.customers = customers;
+        this.status = status;
+        this.items = items;
+    }
+
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Customers getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Customers customers) {
+        this.customers = customers;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public List<OrderItems> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItems> items) {
+        this.items = items;
+    }
+
+    public Integer calculateTotal() {
+        return items.stream()
+                .mapToInt(item -> item.getQuantity() * item.getUnitPriceCents())
+                .sum();
+    }
 }
