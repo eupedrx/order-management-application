a9c5041c902d37f9f4b5a3831b87226484a9b690
Pedro Saturno <pedro1saturno@gmail.com>
2025-12-25 23:31:04 -0300

    change no mapeamento com DTO

--- src/main/java/com/example/testelynx/controller/OrdersController.java
+++ src/main/java/com/example/testelynx/controller/OrdersController.java
@@ -3,6 +3,8 @@ package com.example.testelynx.controller;
 
 import com.example.testelynx.domain.Orders;
 import com.example.testelynx.dto.CreateOrdersDTO;
+import com.example.testelynx.dto.OrderItemResponseDTO;
+import com.example.testelynx.dto.OrderResponseDTO;
 import com.example.testelynx.service.OrdersService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,10 +30,30 @@ public class OrdersController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Orders> buscarOrder(@PathVariable Long id) {
+    public ResponseEntity<OrderResponseDTO> buscarOrder(@PathVariable Long id) {
         return ordersService.findById(id)
+                .map(order -> mapToDTO(order)) // Apenas mapeia
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    private OrderResponseDTO mapToDTO(Orders order) {
+        List<OrderItemResponseDTO> itemsDTO = order.getItems().stream()
+                .map(item -> new OrderItemResponseDTO(
+                        item.getProduct().getId(),
+                        item.getProduct().getName(),
+                        item.getQuantity(),
+                        item.getSubtotal()
+                ))
+                .toList();
+
+        return new OrderResponseDTO(
+                order.getId(),
+                order.getStatus().name(),
+                order.calculateTotal(),
+                order.getCustomers().getId(),
+                itemsDTO
+        );
     }
 
     @PostMapping
